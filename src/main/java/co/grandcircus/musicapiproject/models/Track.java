4a18fb9b62a148b484b47eb9819050646ba56680
Bolanle <bola0321@gmail.com>
2022-07-29 21:36:27 -0400

    created track model

---
+++ src/main/java/co/grandcircus/musicapiproject/models/Track.java
@@ -0,0 +1,34 @@
+package co.grandcircus.musicapiproject.models;
+
+import org.springframework.data.annotation.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Track {
+	@Id
+	private Integer id;
+	private String title;
+	@JsonProperty("release_date")
+	private String releaseDate;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	
+	
+}
